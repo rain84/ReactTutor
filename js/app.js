58b52af03c8898b86280fd1aa50503f742d0870c
rain84 <rainy_sky@mail.ru>
2016-11-15 00:36:12 +0300

    dive into react

--- js/app.js
+++ js/app.js
@@ -2,39 +2,65 @@
  * Created by Aleksandr_Gerasimov on 01-Nov-16.
  */
 
-console.log(React);
-console.log(ReactDOM);
+console.log( React );
+console.log( ReactDOM );
 
 
-let target = document.getElementById('root');
+let my_news = [
+  {
+    author : 'Саша Печкин',
+    text   : 'В четверг, четвертого числа...'
+  },
+  {
+    author : 'Просто Вася',
+    text   : 'Считаю, что $ должен стоить 35 рублей!'
+  },
+  {
+    author : 'Гость',
+    text   : 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+  }
+];
+
+let News = React.createClass( {
+                                render : function () {
+                                  let newsTemplate = this.props.data
+                                    .map( ( item, index ) =>
+                                            (
+                                              <div key={index}>
+                                                <p className="news__author">{item.author}:</p>
+                                                <p className="news__text">{item.text}</p>
+                                              </div>
+                                            )
+                                    );
+
+                                  return (
+                                    <div className="news">
+                                      {newsTemplate}
+                                    </div>
+                                  );
+                                }
+                              } );
+
+let Comments = React.createClass( {
+                                    render : () =>
+                                      <div className="comments">
+                                        Нет новостей - комментировать нечего
+                                      </div>
+                                  } );
+
+let App = React.createClass( {
+                               render : () =>
+                                 <div className="app">
+                                   Всем привет, я компонент App! Я умею отображать новости.
+                                   <News data={my_news}/> {/*comment*/}
+                                   <Comments/>
+                                 </div>
+                             } );
 
 
-let News = React.createClass({
-    render: () =>
-        <div className="news">
-            К сожалению, новостей нет.
-        </div>
-});
-
-let Comments = React.createClass({
-    render: () =>
-        <div className="comments">
-            Нет новостей - комментировать нечего
-        </div>
-});
-
-let App = React.createClass({
-    render: () =>
-        <div className="app">
-            Всем привет, я компонент App! Я умею отображать новости.
-            <News/>
-            <Comments/>
-        </div>
-});
-
-
+let target = document.getElementById( 'root' );
 //noinspection ES6ModulesDependencies
 ReactDOM.render(
-    <App/>,
-    target
+  <App/>,
+  target
 );
